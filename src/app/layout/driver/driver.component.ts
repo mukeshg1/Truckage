28451c23ec2e49c5012347b92102d3034c16b9ac
Mukesh Gupta <mukeshg@mindfiresolutions.com>
2019-03-14 18:08:26 +0530

    UI Improved.

--- src/app/layout/driver/driver.component.ts
+++ src/app/layout/driver/driver.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-driver',
   templateUrl: './driver.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./driver.component.scss'],
   animations: [routerTransition()]
 })
 export class DriverComponent implements OnInit {
-
+  page = 1;
+  size = 10;
   closeResult: string;
 
   driverImgSrc = 'assets/images/faces/face-2.jpg';
@@ -23,6 +25,6 @@ export class DriverComponent implements OnInit {
   }
 
   openVerticallyCentered(content) {
-    this.modalService.open(content, { centered: true, backdropClass: 'light-blue-backdrop' });
+    this.modalService.open(content, {centered: true, backdropClass: 'light-blue-backdrop' });
   }
 }
--- src/app/layout/trips/trips.module.ts
+++ src/app/layout/trips/trips.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { TripsRoutingModule } from './trips-routing.module';
 import { TripsComponent } from './trips.component';
@@ -10,7 +11,8 @@ import { PageHeaderModule } from './../../shared';
   imports: [
     CommonModule,
     TripsRoutingModule,
-    PageHeaderModule
+    PageHeaderModule,
+    NgbModule
   ]
 })
 export class TripsModule { }
